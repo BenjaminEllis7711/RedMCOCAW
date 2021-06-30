a7ecb28db3f546bf8a94c73372b251632bfef31d
Ben Ellis <Benjamin.Ellis7711@gmail.com>
2021-06-30 09:02:47 -0400

    Initial model scaffolding, AllianceMembers model created. May need more data annotations.

--- RedMCOCAW/Models/AllianceMembers.cs
+++ RedMCOCAW/Models/AllianceMembers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace RedMCOCAW.Models
 {
     public class AllianceMembers
     {
+        [Key]
+        public int MemberId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string AllianceTag { get; set; }
+        public string Notes { get; set; }
     }
 }
