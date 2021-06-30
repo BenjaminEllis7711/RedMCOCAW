43e40ad8d0a1962018377224523e3b58133fa599
Ben Ellis <Benjamin.Ellis7711@gmail.com>
2021-06-30 10:19:44 -0400

    Tweaked connection string for db initialization, added data annotations to data layer for relational db

--- RedMCOCAW.Data/Champions.cs
+++ RedMCOCAW.Data/Champions.cs
@@ -7,5 +7,8 @@ namespace RedMCOCAW.Data
 {
     public class Champions
     {
+        public int ChampId { get; set; }
+        public int AllianceKey { get; set; }
+        public string Name { get; set; }
     }
 }
--- RedMCOCAW.Data/Roster.cs
+++ RedMCOCAW.Data/Roster.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace RedMCOCAW.Data
 {
     public class Roster
-    {
+    {       
+        [Key, Column(Order = 0)]
+        public int MemberId { get; set; }
+        [Key, Column(Order = 1)]
+        public int ChampionId { get; set; }
+        [Key, Column(Order = 2)]
+        public int NodeId { get; set; }
     }
 }
