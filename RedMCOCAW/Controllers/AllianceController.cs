98b8e5c4a0d8d2b213ac12273a2b13cc269d0a2a
Ben Ellis <Benjamin.Ellis7711@gmail.com>
2021-07-01 15:02:45 -0400

    Slight changes to alliance index. Starting on alliance edit.

--- RedMCOCAW/Controllers/AllianceController.cs
+++ RedMCOCAW/Controllers/AllianceController.cs
@@ -13,19 +13,48 @@ namespace RedMCOCAW.Controllers
     public class AllianceController : Controller
     {
         // GET: Alliance
-        public ActionResult Index()        
+        public ActionResult Index()
         {
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            var service = new AllianceService(userId);
+            AllianceService service = CreateAllianceService();
             var model = service.GetAlliances();
 
             return View(model);
         }
 
-        // Get: Create
+
+        // Get: Create        
         public ActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AllianceCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);           
+
+            AllianceService service = CreateAllianceService();
+            if (service.CreateAlliance(model))
+            {
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            }
+            else
+                ModelState.AddModelError("", "Alliance could not be created.");
+                return View(model);
+        }
+
+        // Get: Edit
+        public ActionResult Edit(AllianceCreate model)
+        {
+
+        }
+        private AllianceService CreateAllianceService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new AllianceService(userId);
+            return service;
+        }               
     }
 }
